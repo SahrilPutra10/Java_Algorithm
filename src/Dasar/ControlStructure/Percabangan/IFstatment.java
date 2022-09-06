582035157e1c81fe0f0a1d9c9f8795909fee9353
sahrilputra10 <iam.sahrilputra@gmail.com>
2022-09-06 19:09:17 +0700

    adding IFstatment

---
+++ src/Dasar/ControlStructure/Percabangan/IFstatment.java
@@ -0,0 +1,21 @@
+package Dasar.ControlStructure.Percabangan;
+
+public class IFstatment {
+    public static void main(String[] args) {
+        int a = 2; // mendeklarasikan variable a dengan tipe data int
+
+        if (a == 2) { //membuat expresi percabangan if dengan ketentuan jika a sama 'dengan' 2;
+        // jika a sama dengan 2; maka lakukan perintah berikut :
+            System.out.println("Hello World");
+        } else { //merupakan conditional jika data yang dimasukan berbeda dan tidak sama;
+            // jika salah maka jalankan perintah berikut
+            System.out.println("Semangat Dong");
+        }
+    }
+}
+
+/**
+ * ouput program 
+ * 
+ * Hello World
+ */
