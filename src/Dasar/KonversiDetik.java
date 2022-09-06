cc04951b5377d62a43a06d511ced749d98338c8b
sahrilputra10 <iam.sahrilputra@gmail.com>
2022-09-06 16:33:10 +0700

    add KonversiDetik

---
+++ src/Dasar/KonversiDetik.java
@@ -0,0 +1,19 @@
+package Dasar;
+
+import java.util.Scanner;
+
+public class KonversiDetik {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int jam, menit, detik, konversi;
+
+        System.out.println("Masukan Waktu yang ingin di konversikan :");
+        konversi = input.nextInt();
+
+        jam = konversi / 3600;
+        menit = (konversi % 3600) / 60;
+        detik = (konversi % 3600) % 60;
+        System.out.format("Konversi dari : %d detik adalah \n", konversi);
+        System.out.println(jam + " jam, " + menit + " Menit, " + detik + " Detik.");
+    }
+}
