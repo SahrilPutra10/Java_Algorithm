5f6addb8075434f08003087fc7d0c47d9d1f056e
sahrilputra10 <iam.sahrilputra@gmail.com>
2022-09-07 11:54:54 +0700

    add perulangan

--- src/Dasar/ControlStructure/Percabangan/IFstatment.java
+++ src/Dasar/ControlStructure/Percabangan/IFstatment.java
@@ -4,18 +4,19 @@ public class IFstatment {
     public static void main(String[] args) {
         int a = 2; // mendeklarasikan variable a dengan tipe data int
 
-        if (a == 2) { //membuat expresi percabangan if dengan ketentuan jika a sama 'dengan' 2;
-        // jika a sama dengan 2; maka lakukan perintah berikut :
+        if (a == 2) { // membuat expresi percabangan if dengan ketentuan jika a sama 'dengan' 2;
+            // jika a sama dengan 2; maka lakukan perintah berikut :
             System.out.println("Hello World");
-        } else { //merupakan conditional jika data yang dimasukan berbeda dan tidak sama;
-            // jika salah maka jalankan perintah berikut
+        } else { // merupakan conditional statment jika data yang dimasukan berbeda dan tidak
+                 // sama;
+            // jika ada kemungkinan lain maka jalankan perintah berikut
             System.out.println("Semangat Dong");
         }
     }
 }
 
 /**
- * ouput program 
+ * ouput program
  * 
  * Hello World
  */
---
+++ src/Dasar/ControlStructure/Perulangan/WhileStatment.java
@@ -0,0 +1,12 @@
+package Dasar.ControlStructure.Perulangan;
+
+public class WhileStatment {
+    // make star pattern
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 100) {
+            i++;
+            System.out.println(i);
+        }
+    }
+}
