df0eca5ad62140a72f92de6f302096a7ba0c7048
Sahrilputra <55041897+sahrilputra@users.noreply.github.com>
2022-09-07 13:07:02 +0700

    Update WhileStatment.java
    Menambahkan komentar

--- src/Dasar/ControlStructure/Perulangan/WhileStatment.java
+++ src/Dasar/ControlStructure/Perulangan/WhileStatment.java
@@ -1,11 +1,11 @@
 package Dasar.ControlStructure.Perulangan;
 
 public class WhileStatment {
-    // make star pattern
+    // using While Statment to print 1-100
     public static void main(String[] args) {
-        int i = 0;
-        while (i < 100) {
-            i++;
+        int i = 0; //mendeklarasikan variabel i;
+        while (i < 100) { ) //melakukan perulangan "saat i lebih kecil dari 100"
+            i++; //saat i belum memenuhi nilai expressi maka i akan di tambah 1 sampai expresi terpenuhi
             System.out.println(i);
         }
     }
