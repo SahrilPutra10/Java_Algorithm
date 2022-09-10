8a049aa3137eae13e2c0d3647cb6a1b69830abee
sahrilputra <iam.sahrilputra@gmail.com>
2022-09-11 02:10:09 +0700

    adding for loop

--- src/Dasar/ControlStructure/Perulangan/WhileStatment.java
+++ src/Dasar/ControlStructure/Perulangan/WhileStatment.java
@@ -3,9 +3,14 @@ package Dasar.ControlStructure.Perulangan;
 public class WhileStatment {
     // make star pattern
     public static void main(String[] args) {
-        int i = 0;
-        while (i < 100) {
-            i++;
+        int i = 0; // mendeklarasikan variable int a = 0 ;
+
+        while (i < 100) { // melakukan variablel i
+            // melakukan perulangan jika variabel i lebih kecil sama dengan seratus
+            // jika variabel 100< i maka tambah i sebanyak 1X
+            i++; // setiap perulangan do dilakaukan maka jalankan i dan di tambah sebanyak 1
+            // cth. 1 =0 ; tetapi dengan syntax i++ maka setiap do dijalankan maka i akan di
+            // tambah deegnan satu sampai expresi dari while terpenuhi
             System.out.println(i);
         }
     }
